803f7b0b4b0ec6a0e2e668a0b5ef3365efd044ae
heymiriam <miriambellon@gmail.com>
2021-03-10 14:13:20 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
 import axios from 'axios'
-import M from 'materialize-css'
+import M, { Modal } from 'materialize-css'
 import weather from './weather.json'
 
 import Header from './Header'
+
+
 
 class App extends Component {
   constructor() {
@@ -21,21 +23,21 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // const url = `https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
-    // axios.get(url).then((response) => {
-    //   this.setState({
-    //     temp: response.data.main.temp,
-    //   })
-    // })
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
+    axios.get(url).then((response) => {
+       /*this.setState({
+         temp: response.data.main.temp,
+       })*/
+     
     this.setState({
-      temp: weather[0].main.temp,
-      cityName: weather[0].name,
-      weather: weather[0].weather[0].description,
-      high: weather[0].main.temp_max,
-      low: weather[0].main.temp_min,
-      icon: weather[0].weather[0].icon,
+      temp: weather.data.main.temp,
+      cityName: weather.data.name,
+      weather: weather.data.weather[0].description,
+      high: weather.data.main.temp_max,
+      low: weather.data.main.temp_min,
+      icon: weather.data.weather[0].icon,
     })
-
+  })
     var elems = document.querySelectorAll('.modal')
     var instances = M.Modal.init(elems)
   }
@@ -45,30 +47,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header cityName={this.state.cityName} temp={this.state.temp} />
-
         <button data-target='modal1' className='btn modal-trigger'>
           Modal
         </button>
-
-        <div id='modal1' className='modal'>
-          <div className='modal-content'>
-            <h4>{this.state.cityName}</h4>
-            <p>
-              High: {this.state.high} - Low: {this.state.low}
-            </p>
-            <p>
-              {this.state.weather} <img src={iconUrl} alt='icon' />
-            </p>
-          </div>
-          <div className='modal-footer'>
-            <a
-              href='#!'
-              className='modal-close waves-effect waves-green btn-flat'
-            >
-              Close
-            </a>
-          </div>
-        </div>
+        <Modal cityName={this.state.cityName} icon={iconUrl} high={this.state.high} low={this.state.low} weather={this.state.weather} />
+        
       </div>
     )
   }
